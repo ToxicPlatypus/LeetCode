1c0a291b1c2c92904fa5169e9363b7d1dfb750ea
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2023-05-19 21:57:25 +0600

    Update 0136-single-number.js

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -4,7 +4,6 @@
  */
 var singleNumber = function(nums) {
     const hash = new Map();
-    count = 0;
 
     for(let i=0; i<nums.length; i++){
         if(!hash.has(nums[i])){
@@ -16,7 +15,6 @@ var singleNumber = function(nums) {
     }
 
     for(const x of hash.keys()){
-    
         if(hash.get(x) == 1){
             return x;
         }
