7356ee4f491c74619836f63114a90a68af60b013
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2023-05-22 00:31:30 +0600

    Time: 85 ms (85.85%), Space: 59.7 MB (5.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+     let hash = new Map();
+
+    for(let i=0; i<nums.length; i++){
+        if(hash.has(nums[i])){
+            return true;
+        }
+        hash.set(nums[i], i);
+    }
+    return false;
+    
+};
