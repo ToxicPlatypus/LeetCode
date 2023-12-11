3be1193247ce8ef69aaf99a3b46a44248b13ee4e
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2023-12-11 15:43:04 +0600

    Time: 73 ms (12.45%), Space: 44.3 MB (93.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,6 +10,19 @@
  * @param {ListNode} head
  * @return {boolean}
  */
+
+var hasCycle = function(head) {
+    let slow = fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow == fast)
+            return true;
+    }
+    return false;
+};
+
+/*
 var hasCycle = function(head) {
     s = new Set();
     while(head != null){
@@ -19,4 +32,4 @@ var hasCycle = function(head) {
         head = head.next;
     }
     return false;
-};
+}; */
