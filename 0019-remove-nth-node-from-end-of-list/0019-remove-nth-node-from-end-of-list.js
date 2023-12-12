953069f87e447212f7dd1e7356d293ecba944d76
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2023-12-12 16:58:38 +0600

    Time: 64 ms (9.10%), Space: 42.1 MB (91.42%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,28 +10,53 @@
  * @param {number} n
  * @return {ListNode}
  */
+
 var removeNthFromEnd = function(head, n) {
-    count = 1;
-    temp = head;
-    while(temp.next != null){
-        temp = temp.next;
-        count++;
+    root = new ListNode(0);
+    root.next = head;
+    fast = slow = root;
+
+    while(n >= 0){
+        fast = fast.next;
+        n--;
     }
-    if(count == n){
-        newHead = head.next;
-        return newHead;
+
+    while(fast){
+        fast = fast.next;
+        slow = slow.next;
     }
-    else{
-        count = count - n;
-        temp = head;
-        while(temp.next != null){
-            count--;
-            if(count == 0){
-                deleteNode = temp.next;
-                temp.next = temp.next.next;
-                return head;
-            }
-            temp = temp.next;
-        }
-    }
+
+    slow.next = slow.next.next;
+    return root.next;
 };
+
+
+
+
+
+
+// var removeNthFromEnd = function(head, n) {
+//     count = 1;
+//     temp = head;
+//     while(temp.next != null){
+//         temp = temp.next;
+//         count++;
+//     }
+//     if(count == n){
+//         newHead = head.next;
+//         return newHead;
+//     }
+//     else{
+//         count = count - n;
+//         temp = head;
+//         while(temp.next != null){
+//             count--;
+//             if(count == 0){
+//                 deleteNode = temp.next;
+//                 temp.next = temp.next.next;
+//                 return head;
+//             }
+//             temp = temp.next;
+//         }
+//     }
+// };
