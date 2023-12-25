5cb9d0e1111542068f2ef2749b5d8d74b0a28b6f
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2023-12-25 12:41:06 +0600

    Time: 551 ms (100.00%), Space: 93.9 MB (100.00%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.js
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.js
@@ -1,19 +1,27 @@
-var maximizeSquareArea = function(m, n, hFences, vFences) {
-  hFences.push(1), hFences.push(m);
-  vFences.push(1), vFences.push(n);
-  let hDiffs = new Set();
+var maximizeSquareArea = function (m, n, hFences, vFences) {
+  hFences.push(1);
+  hFences.push(m);
+  vFences.push(1);
+  vFences.push(n);
+
+  let map = new Set();
+
   for (let i = 0; i < hFences.length; i++) {
     for (let j = i + 1; j < hFences.length; j++) {
-      let diff = Math.abs(hFences[i] - hFences[j]);
-      hDiffs.add(diff);
+      let difference = Math.abs(hFences[i] - hFences[j]);
+      map.add(difference);
     }
   }
-  let maxDiff = 0;
+  let ans = 0;
+
   for (let i = 0; i < vFences.length; i++) {
     for (let j = i + 1; j < vFences.length; j++) {
-      let diff = Math.abs(vFences[i] - vFences[j]);
-      if (hDiffs.has(diff)) maxDiff = Math.max(maxDiff, diff);
+      let dif = Math.abs(vFences[i] - vFences[j]);
+      if (map.has(dif)) {
+        ans = Math.max(ans, dif);
+      }
     }
   }
-  return maxDiff === 0 ? -1 : (BigInt(maxDiff) * BigInt(maxDiff)) % 1000000007n;
+
+  return ans === 0 ? -1 : (BigInt(ans) * BigInt(ans)) % 1000000007n;
 };
