4fba951f40ad54463aa4baece5f3358559ca85f3
Mohammad Rafid Rabbi <rafidrabbi123@gmail.com>
2024-05-28 13:55:08 +0600

    Time: 56 ms (75.00%), Space: 211.2 MB (20.00%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.rb
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.rb
@@ -1,59 +1,26 @@
-class Queue
-  def initialize
-    @elements = []
-  end
-
-  def push(value)
-    @elements << value
-  end
-
-  def peek
-    empty? ? nil : @elements[0]
-  end
-
-  def pop
-    @elements.shift unless empty?
-  end
-
-  def size
-    @elements.length
-  end
-
-  def empty?
-    @elements.length == 0
-  end
-end
-
-
 class MyStack
-  attr_reader :queue1, :queue2
-
   def initialize()
-    @queue1 = Queue.new
-    @queue2 = Queue.new
+    @queue = []
   end
 
   def push(x)
-     while !queue1.empty?
-       queue2.push(queue1.pop)
-     end
-     
-     queue1.push(x)
+    @queue << x
 
-     while !queue2.empty?
-      queue1.push(queue2.pop)
-     end
+    for i in 0...(@queue.length - 1)
+      @queue << @queue.shift
+    end
+
   end
 
   def pop()
-     queue1.pop
+    @queue.shift
   end
 
   def top()
-    queue1.peek
+    @queue[0]
   end
 
   def empty()
-    @queue1.empty? && @queue2.empty?
+    @queue.empty?
   end
 end
